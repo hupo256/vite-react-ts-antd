ead7e74872ed8250d3789d9e2c5e3d7dc1b40e0c
童冬冬 <hupo256@qq.com>
2021-08-05 17:09:18 +0800

    create files

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <p>Hello Vite + React!</p>
+    </div>
+  )
+}
+
+export default App
